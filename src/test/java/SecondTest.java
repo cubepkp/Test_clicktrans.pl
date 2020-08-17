f5517cabf8291ab85ffc417488faee4908d0bef5
BArthas <cubepkp@gmail.com>
2020-08-17 21:35:38 +0200

    v2

--- src/test/java/SecondTest.java
+++ src/test/java/SecondTest.java
@@ -1,18 +1,24 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+import org.testng.annotations.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+public class SecondTest {
+    FirefoxDriver driver;
 
-
-class TestTest {
-    public static void main(String[] args) {
+    @BeforeTest
+    public void Setup() {
 
         System.setProperty("webdriver.gecko.driver", "C:/Users/bart/Desktop/SeleniumBDD/geckodriver.exe");
-        FirefoxDriver driver = new FirefoxDriver();
-        driver.manage().window().maximize();
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void testCase() {
+
         driver.get("https://dev-1.clicktrans.pl/register-test/courier");
-
-
         driver.findElement(By.xpath("//input[@id='user_register_company_name']")).sendKeys("Usługi Transportowe");
         driver.findElement(By.xpath("//input[@id='user_register_email']")).sendKeys("jan.kowalski@email.com");
         driver.findElement(By.xpath("//input[@id='user_register_name']")).sendKeys("Jan Kowalski");
@@ -25,10 +31,12 @@ class TestTest {
 
         String success_message = driver.findElement(By.xpath("//div[@class='ui success message']")).getText();
         assertThat(success_message).isEqualTo("OK - some registration logic is mocked");
-
-
-        driver.quit();
-
-
+        //assertThat(success_message).isEqualTo("OK - some registration logic is mocke");
     }
+
+    @AfterTest
+    public void closeBrowser() {
+        driver.quit();
+    }
+
 }
